3d786e32a70c7319405edc0fd528896f01513547
Lisa Jung <lisahjung@gmail.com>
2020-03-23 18:21:34 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -16,20 +16,21 @@ end
 def monopoly_with_third_tier
     hash ={:railroads => {
     :railroads => {
-      :pieces =>400
+      :pieces = >400,
       :rent_in_dollars => {
         :one_piece_owned => 25,
         :two_pieces_owned => 50,
         :three_pieces_owned => 100,
         :four_pieces_owned => 200
-      }
+      },
       :names => {
-        :reading_railroad => {}
-        
+        :reading_railroad => {},
+        :pennsylvania_railroad => {},
+        :b_and_o_railroad => {},
+        :shortline_railroad => {}
       }
     }
    }
-  }
 end
   
 
