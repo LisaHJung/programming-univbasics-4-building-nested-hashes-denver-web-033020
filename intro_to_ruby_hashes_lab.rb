7e6e2765b0600faeb175b08bdd62c62fce98650b
Lisa Jung <lisahjung@gmail.com>
2020-03-23 18:39:00 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,30 +1,25 @@
-=begin
 def base_hash
  hash ={:railroads => {}}
 end
 
 def monopoly_with_second_tier
    hash ={:railroads => {
-    :railroads => {
       :pieces => 4, 
       :rent_in_dollars => {},
       :names => {}
     }
-   }
   }
 end
 
 
 def monopoly_with_third_tier
     hash = {:railroads => {
-    :railroads => {
       :pieces => 4,
       :rent_in_dollars => {
         :one_piece_owned => 25,
         :two_pieces_owned => 50,
         :three_pieces_owned => 100,
         :four_pieces_owned => 200
-      }
       },
       :names => {
         :reading_railroad => {},
@@ -38,14 +33,12 @@ end
 
 def monopoly_with_fourth_tier
     hash = {:railroads => {
-    :railroads => {
       :pieces => 4,
       :rent_in_dollars => {
         :one_piece_owned => 25,
         :two_pieces_owned => 50,
         :three_pieces_owned => 100,
         :four_pieces_owned => 200
-      }
       },
       :names => {
         :reading_railroad => {
@@ -95,7 +88,7 @@ end
 
 
 
-
+=begin
 def base_hash
   hash={:railroads => {}}
 end
@@ -145,3 +138,4 @@ def monopoly_with_fourth_tier
    }
  }
 end
+=end
