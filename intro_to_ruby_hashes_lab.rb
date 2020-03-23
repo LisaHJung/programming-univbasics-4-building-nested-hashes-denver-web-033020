aa410454b903e4222c529b7c0ea5e1434fc76956
Lisa Jung <lisahjung@gmail.com>
2020-03-23 18:35:57 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -5,7 +5,7 @@ end
 def monopoly_with_second_tier
    hash ={:railroads => {
     :railroads => {
-      :pieces => 4,
+      :pieces => 4, 
       :rent_in_dollars => {},
       :names => {}
     }
