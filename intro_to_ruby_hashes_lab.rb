28fa5718236bd32641df25d30aebc100d8939b17
Lisa Jung <lisahjung@gmail.com>
2020-03-23 18:05:38 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,18 +1,20 @@
 def base_hash
-  {
-    :railroads =>{}
-  }
+ hash ={:railroads => {}}
 end
 
 def monopoly_with_second_tier
-   {
+   hash ={:railroads => {
     :railroads =>{
       :pieces =>400
       :rent_in_dollars => {}
       :names => {}
     }
+   }
   }
+end
 
+def monopoly_with_third_tier
+  
 
 
 
